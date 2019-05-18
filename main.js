023c01feea094edbaa3e2b6c896df89d538dada7
Hassen Ghanmi <hasoob2@gmx.com>
2019-05-18 21:59:18 +0100

    updating game logic

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-var gameRoundTrueCells = [];
-var gameDOMCells = []
-var gameMediaElements = []
-var gameStage = 1;
-var playBtn;
-var score = 0;
-var topScore = window.localStorage.getItem("hassenSimonTopScore");
+var gameData = {
+    gameAICells: [],
+    gameDOMCells: [],
+    gameRemainingCellsCount: 0,
+    gameMediaElements: [],
+    gameStage: 0,
+    playBtn: undefined,
+    score: 0,
+    topScore: window.localStorage.getItem("hassenSimonTopScore") || 0
+}
+
+var miscellaneous
+var gameEventHandlers = {
+    /**
+    * play button click event hanldner
+    * @param {*} event click event? meh
+    */
+    playBtnEventHandler: function(event){
+        ready(gameData.gameDOMCells);
+    },
+    gameCellClickEventHandler: function(event){
+        gameData.gameRemainingCellsCount++;
+        let cellId = parseInt(event.target.dataset.cellId);
+        let rightCellId = gameData.gameAICells[gameData.gameRemainingCellsCount - 1];
+        console.log("current cellId = ", cellId);
+        console.log("right cellId = ", rightCellId);
+        console.log("current cell count: ", gameData.gameRemainingCellsCount, " / ", gameData.gameAICells.length);
+        playAudio(cellId);
+        if(cellId != rightCellId){
+            gameOver();
+        }
+        else if(gameData.gameRemainingCellsCount >= gameData.gameAICells.length ){//stage complete
+            gameNextStage();
+        }
+        //console.log('cell: ', event.target);
+    }
+}
+
 
 /** Change game title text configurationally
  * 
@@ -36,82 +67,93 @@ function gameTitleMessages(messagesArray){
  * @param gameDOMCells: Array of DOM elelements corresponding to game cells
  */
 function ready(gameDOMCells){
-    gameRoundTrueCells.push(Math.floor(Math.random() * 4) + 1);
+    gameData.gameStage++;
+    gameData.gameAICells.push(Math.floor(Math.random() * 4) + 1);
     gameDOMCells.forEach(element => {
         element.classList.add("disabled");
     });
-    playBtn.disabled = true;
+    gameData.playBtn.disabled = true;
     gameTitleMessages([
         {text:'Ready', fontWeight: 'bold', delay:1000},
         {text:'Go', fontWeight: 'bold', delay: 1000},
-        {text:'Simon', fontWeight: '', callback: gameAI, callbackParams: [gameDOMCells, gameRoundTrueCells]}
+        {text:'Simon', fontWeight: '', callback: gameAI, callbackParams: [gameDOMCells, gameData.gameAICells]}
     ]);
 }
 
 /**
  * the AI of the game in view mode
  * @param {*} gameDOMCells Array of DOM elelements corresponding to game cells
- * @param {*} arr Array of cell indexes to be highlighted
+ * @param {*} cellsArray Array of cell indexes to be highlighted
  */
-function gameAI(gameDOMCells, arr){
-    if(arr.length){
-        gameDOMCells[arr[0] - 1].classList.add("active")
-        playAudio(arr[0] - 1)
+function gameAI(gameDOMCells, cellsArray){
+    if(cellsArray.length){
+        gameDOMCells[cellsArray[0] - 1].classList.add("active")
+        playAudio(cellsArray[0])
         let cellActiveTimer = window.setTimeout(()=>{
-            gameDOMCells[arr[0] - 1].classList.remove("active");
+            gameDOMCells[cellsArray[0] - 1].classList.remove("active");
             let nextRecursionTimer = window.setTimeout(()=>{
-                gameAI(gameDOMCells, arr.slice(1));
+                gameAI(gameDOMCells, cellsArray.slice(1));
             }, 400);
         }, 350);
     }
     else{
         gameTitleMessages([
-        {text:'Your turn!', fontWeight: 'bold', delay: 1000},
-        {text:'Simon', fontWeight: '', callback: ()=>{
+        {text:'Your turn!', fontWeight: 'bold',callback: ()=>{
             userGamePlay(gameDOMCells)
-            //playBtn.disabled = false;    //nope :p
-        } , callbackParams: [gameDOMCells]
-        }
+            gameData.playBtn.disabled = false;    //nope :p
+        } , callbackParams: [gameDOMCells]},
         ]);
     }
 }
 
 function userGamePlay(gameDOMCells){
+    gameData.gameRemainingCellsCount = 0; // reset
     gameDOMCells.forEach(element => {
         element.classList.remove("disabled");
+        element.addEventListener("click", gameEventHandlers.gameCellClickEventHandler);
     });
+
 }
 
-/**
- * .playBtn's click event hanldner
- * @param {*} event click event? meh
- */
-function playBtnEventHandler(event){
-    ready(gameDOMCells);
+function gameNextStage(){
+    gameData.playBtn.removeEventListener("click", gameEventHandlers.gameCellClickEventHandler);
+    gameData.gameDOMCells.forEach(element => {
+        element.classList.add("disabled");
+    });
+    console.log("gameNextStage !")
 }
+
+function gameOver(){
+    gameData.playBtn.removeEventListener("click", gameEventHandlers.gameCellClickEventHandler);
+    gameData.gameDOMCells.forEach(element => {
+        element.classList.add("disabled");
+    });
+    console.log("gameOver !")
+}
+
 
 /**
  * Play one of simon game's beeps according to the clicked cell
- * @param {*} nth audio element index equivalent to cell index
+ * @param {*} id audio element index equivalent to cell index
  */
-function playAudio(nth){
-    gameMediaElements[nth].cloneNode(true).play();
+function playAudio(id){
+    gameData.gameMediaElements[id - 1].cloneNode(true).play();
 }
 ///////////////////////////
 
 document.addEventListener("DOMContentLoaded", function(){
-    gameMediaElements.push(document.querySelector("#simon-sound-1"));
-    gameMediaElements.push(document.querySelector("#simon-sound-2"));
-    gameMediaElements.push(document.querySelector("#simon-sound-3"));
-    gameMediaElements.push(document.querySelector("#simon-sound-4"));
+    gameData.gameMediaElements.push(document.querySelector("#simon-sound-1"));
+    gameData.gameMediaElements.push(document.querySelector("#simon-sound-2"));
+    gameData.gameMediaElements.push(document.querySelector("#simon-sound-3"));
+    gameData.gameMediaElements.push(document.querySelector("#simon-sound-4"));
 
-    gameDOMCells.push(document.querySelector(".cell:nth-child(1)"));
-    gameDOMCells.push(document.querySelector(".cell:nth-child(2)"));
-    gameDOMCells.push(document.querySelector(".cell:nth-child(3)"));
-    gameDOMCells.push(document.querySelector(".cell:nth-child(4)"));
-
-    playBtn = document.querySelector(".gameBtn.playBtn");
-    playBtn.addEventListener("click", playBtnEventHandler);
+    gameData.gameDOMCells.push(document.querySelector(".cell[data-cell-id='1']"));
+    gameData.gameDOMCells.push(document.querySelector(".cell[data-cell-id='2']"));
+    gameData.gameDOMCells.push(document.querySelector(".cell[data-cell-id='3']"));
+    gameData.gameDOMCells.push(document.querySelector(".cell[data-cell-id='4']"));
+    
+    gameData.playBtn = document.querySelector(".gameBtn.playBtn");
+    gameData.playBtn.addEventListener("click", gameEventHandlers.playBtnEventHandler);
 
     
     //var pauseBtn = document.querySelector(".gameBtn.pauseBtn");
